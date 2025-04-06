bd9f3dbdb9f2f5660e3a2d187821af2ed844b46c
AleksandarDimitrov1233 <train.sirene@gmail.com>
2025-04-06 18:49:00 +0300

    Update Main.java
    Добавени опции за меню.

--- Main.java
+++ Main.java
@@ -3,7 +3,18 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-           ArrayList<ArrayList<Object>> spreadsheet = new ArrayList<ArrayList<Object>>();
+           ArrayList <Object> spreadsheet = new ArrayList <Object>();
            Scanner scanner = new Scanner(System.in);
+
+        int size;
+        System.out.println("How many values do you want to add?");
+        size = scanner.nextInt();
+        System.out.println("1: Enter an integer:");
+        System.out.println("2: Enter a double number:");
+        System.out.println("3: Enter a string:");
+        System.out.println("4: Enter a formula:");
+        System.out.println("5: Edit a number:");
+        System.out.println("6: Print all data:");
+
     }
 }
