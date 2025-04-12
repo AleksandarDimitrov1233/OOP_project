d81b485428781ddb0c73c34276df086a13b69527
AleksandarDimitrov1233 <train.sirene@gmail.com>
2025-04-12 18:27:11 +0300

    Update Main.java
    Добавена меню опция за добавяне на целочислена стойност.

--- Main.java
+++ Main.java
@@ -1,20 +1,32 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 import java.util.ArrayList;
-
 public class Main {
     public static void main(String[] args) {
-           ArrayList <Object> spreadsheet = new ArrayList <Object>();
-           Scanner scanner = new Scanner(System.in);
+        ArrayList<ArrayList<Object>> spreadsheet = new ArrayList<ArrayList<Object>>;
+        Scanner scanner = new Scanner(System.in);
+        int choice = 1;
+        while (choice!=7) {
+            System.out.println("1: Enter an integer:");
+            System.out.println("2: Enter a double number:");
+            System.out.println("3: Enter a string:");
+            System.out.println("4: Enter a formula:");
+            System.out.println("5: Edit a number:");
+            System.out.println("6: Print all data:");
+            System.out.println("7: Exit");
+            System.out.println("Enter your choice: ");
+            choice = scanner.nextInt();
 
-        int size;
-        System.out.println("How many values do you want to add?");
-        size = scanner.nextInt();
-        System.out.println("1: Enter an integer:");
-        System.out.println("2: Enter a double number:");
-        System.out.println("3: Enter a string:");
-        System.out.println("4: Enter a formula:");
-        System.out.println("5: Edit a number:");
-        System.out.println("6: Print all data:");
-
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter an integer: ");
+                    int intValue = scanner.nextInt();
+                    scanner.nextLine();
+                    ArrayList<Object> intRow = new ArrayList<>();
+                    intRow.add(intValue);
+                    spreadsheet.add(intRow);
+                    break;
+            }
+        }
     }
 }
