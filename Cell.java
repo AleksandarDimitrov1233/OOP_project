4ef546e27f5b9e5eb87158bce42e7fc8f27c6ea6
AleksandarDimitrov1233 <train.sirene@gmail.com>
2025-06-20 17:53:10 +0300

    Update Cell.java

--- Cell.java
+++ Cell.java
@@ -1,3 +1,134 @@
-abstract class Cell {
-    
+package TableUtils;
+
+import ExpressionEngine.ExpressionEngine;
+
+public class Cell {
+    public enum Type {
+        INT, DECIMAL, STR, NONE, EXPR
+    }
+
+    private final Type type;
+    private final Integer intValue;
+    private final Float decimalValue;
+    private final String strValue;
+    private final String exprValue;
+
+    private Cell(Type type,
+            Integer intValue,
+            Float decimalValue,
+            String strValue,
+            String exprValue) {
+        this.type = type;
+        this.intValue = intValue;
+        this.decimalValue = decimalValue;
+        this.strValue = strValue;
+        this.exprValue = exprValue;
+    }
+
+    public static Cell ofInt(int v) {
+        return new Cell(Type.INT, v, null, null, null);
+    }
+
+    public static Cell ofDecimal(Float v) {
+        return new Cell(Type.DECIMAL, null, v, null, null);
+    }
+
+    public static Cell ofStr(String v) {
+        return new Cell(Type.STR, null, null, v, null);
+    }
+
+    public static Cell ofExpr(String v) {
+        return new Cell(Type.EXPR, null, null, null, v);
+    }
+
+    public static Cell none() {
+        return new Cell(Type.NONE, null, null, null, null);
+    }
+
+    public boolean isInt() {
+        return type == Type.INT;
+    }
+
+    public boolean isDecimal() {
+        return type == Type.DECIMAL;
+    }
+
+    public boolean isStr() {
+        return type == Type.STR;
+    }
+
+    public boolean isExpr() {
+        return type == Type.EXPR;
+    }
+
+    public boolean isNone() {
+        return type == Type.NONE;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public String toString(Boolean serializing) {
+        switch (type) {
+            case INT:
+                return intValue.toString();
+            case DECIMAL:
+                return decimalValue.toString();
+            case STR:
+                return strValue;
+            case EXPR:
+                if (serializing && exprValue.charAt(0) != '\'') {
+                    String serializedExpr = "\'" + exprValue + "\'";
+                    return serializedExpr;
+                }
+                return exprValue;
+            default:
+                return "";
+        }
+    }
+
+    public String toStringOnPrint() {
+        switch (type) {
+            case INT:
+                return intValue.toString();
+            case DECIMAL:
+                return decimalValue.toString();
+            case STR:
+                return strValue;
+            case EXPR:
+                String removeSingleQuotes = exprValue.replace("'", "");
+                String filtered = removeSingleQuotes.substring(removeSingleQuotes.indexOf('R'));
+                Float result = ExpressionEngine.evaluate(filtered);
+                if (result == null) {
+                    return "ERROR";
+                } else {
+                    return result.toString();
+                }
+            default:
+                return "";
+        }
+    }
+
+    public static Cell parse(String s) {
+        if (s == null || s.isEmpty()) {
+            return Cell.none();
+        }
+        if (s.charAt(0) == '\'' && s.charAt(1) == '=' || s.charAt(0) == '=') {          
+            return Cell.ofExpr(s);
+        }
+        try {
+            return Cell.ofInt(Integer.parseInt(s));
+        } catch (NumberFormatException nfe) {
+        }
+        try {
+            return Cell.ofDecimal(Float.parseFloat(s));
+        } catch (NumberFormatException nfe) {
+        }
+        if (s.charAt(0) == '"' && s.charAt(s.length() - 1) == '"') {
+            return Cell.ofStr(s);
+        }
+        System.out.println(s + " is an invalid data type");
+        return null;
+    }
 }
