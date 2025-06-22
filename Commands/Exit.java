9df72e803a0371a9ea361a3003aec518733603cd
AleksandarDimitrov1233 <train.sirene@gmail.com>
2025-06-22 14:10:56 +0300

    Update Exit.java
    Добавено пояснение за потребителя

--- Commands/Exit.java
+++ Commands/Exit.java
@@ -4,7 +4,7 @@ public class Exit extends Command {
     @Override
     public void execute(String[] args) {
         if (gTable == null) {
-        System.out.println("Table not open");
+        System.out.println("Table not open. Please use open <file_name>.");
         return;
     }
         System.exit(0);
