a11f6a55250a4dadf4ec3eeef422cae64b057d01
AleksandarDimitrov1233 <train.sirene@gmail.com>
2025-06-22 22:30:49 +0300

    Update Exit.java
    Махната проверка за отворен файл

--- Commands/Exit.java
+++ Commands/Exit.java
@@ -3,10 +3,6 @@ package Commands;
 public class Exit extends Command {
     @Override
     public void execute(String[] args) {
-        if (gTable == null) {
-        System.out.println("Table not open. Please use open <file_name>.");
-        return;
-    }
         System.exit(0);
     }
 }
