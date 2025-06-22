e9c7d1c3c26da4a201a1cf2396b9fb78adac782b
AleksandarDimitrov1233 <train.sirene@gmail.com>
2025-06-22 14:24:54 +0300

    Update Print.java
    Добавено пояснение за потребителя

--- Commands/Print.java
+++ Commands/Print.java
@@ -6,7 +6,7 @@ public class Print extends Command {
     @Override
     public void execute(String[] args) {
          if (gTable == null) {
-        System.out.println("Table not open");
+        System.out.println("Table not open. Please use open <file_name>.");
         return;
     }
         Table.prettyPrint(gTable.table);
